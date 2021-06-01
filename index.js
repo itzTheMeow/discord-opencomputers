f80b41a64ccc3c6d7451a9cfb9fd68ab8513d872
Meow <ez@clap.com>
2021-06-01 10:20:09 +0200

    dribble now bruh

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 	res.json({ success: true });
 });
 app.post('/login', (req, res) => {
-	console.log('Request: ', JSON.stringify(req));
+	console.log('Request: ', req.get('Content-Type'));
 	let bot = new Discord.Client();
 	bot.on('ready', () => {
 		res.json({
